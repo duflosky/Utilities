31a17bdeabd0d8aa3e71244706c84de890becaa7
Maxime <32262840+duflosky@users.noreply.github.com>
2023-11-10 15:36:35 +0100

    feat: update Pool;

--- Assets/Sources/Pools/Pool.cs
+++ Assets/Sources/Pools/Pool.cs
@@ -1,35 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class Pool<T> where T : Object
 {
-    private readonly Queue<T> pool = new Queue<T>();
+    private readonly Queue<T> pool = new();
     private readonly T defaultItem;
 
-    public Pool(T defaultItem)
+    public Pool(T defaultItem, int count = 0, Action<T> callback = null)
     {
         this.defaultItem = defaultItem;
+        for (int i = 0; i < count; i++)
+        {
+            var currentElement = Object.Instantiate(defaultItem);
+            AddToPool(currentElement, callback);
+        }
     }
 
-    public void AddToPool(T toAdd)
+    public void AddToPool(T toAdd, Action<T> callback = null)
     {
         if (toAdd is GameObject t)
         {
             t.SetActive(false);
         }
+
         pool.Enqueue(toAdd);
     }
 
-    public T GetFromPool()
+    public T GetFromPool(Action<T> callback = null)
     {
-        return pool.Count > 0 ? pool.Dequeue() : Object.Instantiate(defaultItem);
+        T element = pool.Count > 0 ? pool.Dequeue() : Object.Instantiate(defaultItem);
+
+        if (element is GameObject t)
+        {
+            t.SetActive(true);
+        }
+        else if (element is Component mono)
+        {
+            mono.gameObject.SetActive(true);
+        }
+
+        callback?.Invoke(element);
+        return element;
     }
 
     public IEnumerator AddToPoolLater(T item, float lifeTime)
     {
         yield return new WaitForSeconds(lifeTime);
         pool.Enqueue(item);
-        
     }
 }
